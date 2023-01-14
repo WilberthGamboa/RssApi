980e0de20ec1f00123768036036bef875961c572
Your Name <you@example.com>
2023-01-14 13:03:29 -0600

    agregar rss a db

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,11 +2,13 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { RssModule } from './rss/rss.module';
-
 import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
-  imports: [RssModule],
+  imports: [RssModule,
+  MongooseModule.forRoot('')
+  
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
