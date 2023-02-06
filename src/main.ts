7948bc68601fc3230443a5b45e0e5995230ce3a8
wilberth <wgcampos15@gmail.com>
2023-02-05 22:42:32 -0600

    change cors

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,10 @@
+/* eslint-disable prettier/prettier */
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule,{cors:false});
 
   app.useGlobalPipes(
     new ValidationPipe({
