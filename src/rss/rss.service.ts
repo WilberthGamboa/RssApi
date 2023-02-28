1225d5770e713e57393b0fb82a1d21d8329f067e
Your Name <you@example.com>
2023-02-28 16:56:19 -0600

    ola pimpollo

--- src/rss/rss.service.ts
+++ src/rss/rss.service.ts
@@ -44,14 +44,15 @@ export class RssService {
       noticiasExample.idRss=rssCreado.id;
       noticiasExample.titulo = item.title;
       //cambiamos la fecha
-      noticiasExample.descripcion = item.content;
+      noticiasExample.descripcion = item.summary;
       const formatoFecha = new Date(item.pubDate).toLocaleDateString();
       noticiasExample.fecha = formatoFecha;
       noticiasExample.categorias = item.categories;
-   
+      noticiasExample.html2 = item.contentSnippet;
+      console.log(item.content);
       noticiasExample.url=  item.link;
-      noticiasExample.html= item.content.toString();
-     noticiasExample.html2 = item.contentSnippet.toString();
+      noticiasExample.html= item.content;
+    
       
      
       this.noticiasService.create(noticiasExample);
