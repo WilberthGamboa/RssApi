95824d7769980f27df7d28ec6e25d278490115bf
WilberthGamboa <wgcampos15@gmail.com>
2023-04-21 02:09:21 -0600

    ts

--- src/main.ts
+++ src/main.ts
@@ -1,29 +1,22 @@
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import cacheController from 'express-cache-controller'; // <--- Agregar esta línea
+import * as cacheController from 'express-cache-controller';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule,{cors:true});
   app.enableCors({
-  allowedHeaders: '*',
-  origin: '*',
-  credentials: true,
-});
+    allowedHeaders: '*',
+    origin: '*',
+    credentials: true,
+  });
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist:true,
       forbidNonWhitelisted:true
     })
   );
-
-  // Agregar configuración de cache
-  app.use(cacheController({
-    maxAge: 60, // Tiempo máximo de vida de la caché (en segundos)
-    sMaxAge: 60, // Tiempo máximo de vida compartido (en segundos)
-    noCache: true, // Deshabilitar la caché por completo si es verdadero
-  }));
-
+  app.use(cacheController());
   await app.listen(process.env.PORT);
 }
 bootstrap();
