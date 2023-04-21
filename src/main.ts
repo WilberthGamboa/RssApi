298b735e644395453504aa8f9b42ab1a3e66c55f
WilberthGamboa <wgcampos15@gmail.com>
2023-04-21 01:41:28 -0600

    change cache

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,7 @@
-/* eslint-disable prettier/prettier */
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import cacheController from 'express-cache-controller'; // <--- Agregar esta línea
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule,{cors:true});
@@ -15,7 +15,15 @@ async function bootstrap() {
       whitelist:true,
       forbidNonWhitelisted:true
     })
-  )
+  );
+
+  // Agregar configuración de cache
+  app.use(cacheController({
+    maxAge: 60, // Tiempo máximo de vida de la caché (en segundos)
+    sMaxAge: 60, // Tiempo máximo de vida compartido (en segundos)
+    noCache: true, // Deshabilitar la caché por completo si es verdadero
+  }));
+
   await app.listen(process.env.PORT);
 }
 bootstrap();
